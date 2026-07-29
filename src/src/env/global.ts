c3589d14355f2334d891c315325e60b3430e51f2
Pons Emilien <emilien.pons@etu.iut-tlse3.fr>
2026-07-30 00:24:15 +0200

    added errors for wrong numb of args of global funcs tabs work automatic parents fix code format syntax highlighting (kind of)

--- src/src/env/global.ts
+++ src/src/env/global.ts
@@ -1,3 +1,5 @@
+import {stringify} from "@interpreter";
+
 export const global_env = {
     '+':      (args: any[]) => args.reduce((a, b) => a + b, 0),
     '-':      (args: any[]) => args.length === 1 ? -args[0] : args.reduce((a, b) => a - b),
@@ -12,11 +14,29 @@ export const global_env = {
     '>=':     (args: any[]) => args[0] >= args[1],
     '<=':     (args: any[]) => args[0] <= args[1],
     '!':      (args: any[]) => !args[0],
-    'get':    (args: any[]) => args[0][args[1]] ?? null,
-    'length': (args: any[]) => args[0].length,
-    'push':   (args: any[]) => [...args[0], ...args.slice(1)],
+    'echo':   (args: any[]) =>
+              {
+                  if (args.length < 1) throw new Error('Expected 1 argument (echo <value>');
+                  return args.map(arg => stringify(arg)).join(' ');
+              },
+    'get':    (args: any[]) =>
+              {
+                  if (args.length < 2) throw new Error('Expected 2 arguments (get <list> <index>)');
+                  return args[0][args[1]] ?? null
+              },
+    'length': (args: any[]) =>
+              {
+                  if (args.length < 1) throw new Error(`Expected 1 argument (length <value>)`);
+                  return args[0].length
+              },
+    'push':   (args: any[]) =>
+              {
+                  if (args.length < 2) throw new Error(`Expected 2 arguments (push <list> <value>)`);
+                  return [...args[0], ...args.slice(1)]
+              },
     'typeof': (args: any[]) =>
               {
+                  if (args.length < 1) throw new Error(`Expected 1 argument (typeof <value>)`);
                   const val = args[0];
                   if (val === null || val === undefined) return '@null';
                   if (Array.isArray(val)) return 'list';
